0f5030187fb91c9d330606b84a259e7c7a590284
Maldon Meehan and Molly McGlone <maldon+molly@>
2016-09-14 15:10:20 -0700

    add user id to image table and unit tests for image and user relationship

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
+  belongs_to :user
   has_attached_file :image, :styles => { :medium => "500x500>"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
