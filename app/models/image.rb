a7ea2233084d5d3573048c3e2c0a56e5d35946b5
Molly <mollykmcglone@gmail.com>
2016-09-15 17:24:17 -0700

    resize images and add bootstrap columns for better display on images index page

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   has_and_belongs_to_many :tags
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "500x500>"}
+  has_attached_file :image, :styles => { :medium => "350x350#"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :title, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tags
   has_many :images
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
